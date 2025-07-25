df271877fb49161d87b433bf00a31ab9ed7473f6
Kreliannn <krelianquimson@gmail.com>
2025-07-25 15:02:25 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 "use client"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [isGreen, setIsGreen] = useState(true)
 
 
   const handleClick = () => {
     alert("printing")
+    setIsGreen((prev) => !prev)
   }
 
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
+
+      <div className={`${isGreen ? "bg-green-500" : "bg-red-500"} w-[100px] h-[100px]`}>
+
+      </div>
+
       <button onClick={handleClick} className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors duration-200" >
         Print 
       </button>
