84bb8533a53988fe7dbb51c4944aeeeec23395ba
Kreliannn <krelianquimson@gmail.com>
2025-07-25 15:35:40 +0800

    update 2

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,8 @@ declare global {
 
 export default function Home() {
 
-  const [isGreen, setIsGreen] = useState(true)
-
-
+  
   const handleClick = () => {
-    alert("printing")
-    setIsGreen((prev) => !prev)
     if (window.Android && window.Android.triggerPrint) {
       window.Android.triggerPrint();
     }
@@ -27,9 +23,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
 
-      <div className={`${isGreen ? "bg-green-500" : "bg-purple-500"} w-[100px] h-[100px]`}>
 
-      </div>
 
       <button onClick={handleClick} className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors duration-200" >
         Print 
