523858ee5d56b394d0281e6a37786485b499aa55
Kreliannn <krelianquimson@gmail.com>
2025-07-27 19:08:58 +0800

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#00cc66" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
