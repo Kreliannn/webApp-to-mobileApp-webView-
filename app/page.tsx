e8978b2bc85d564f68512b7ee2e50497912829e9
Kreliannn <krelianquimson@gmail.com>
2025-08-12 21:43:36 +0800

    back to old code

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,37 @@
-"use client";
-import { useEffect, useState } from "react";
+"use client"
+import { useState } from "react";
+
+declare global {
+  interface Window {
+    Android: {
+      triggerPrint: () => void;
+    };
+  }
+}
+
 
 export default function Home() {
-  const [printerReady, setPrinterReady] = useState(false);
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://oss-imin.s3.ap-southeast-1.amazonaws.com/printer_sdk/JSPrinterSDK.js";
-    script.async = true;
-    script.onload = () => setPrinterReady(true);
-    document.body.appendChild(script);
-  }, []);
+  const [isGreen, setIsGreen] = useState(true)
 
-  const handlePrint = (type: "USB" | "SPI" | "Bluetooth") => {
-    const printer = (window as any).IminPrintInstance;
-    if (printerReady && printer) {
-      alert(`Trying with ${type}...`);
-      printer.initPrinter(printer.PrintConnectType[type]);
-      printer.setAlignment(1);
-      printer.setTextSize(28);
-      printer.printText(`Testing ${type} connection...\n\n`);
-      printer.partialCut();
-    } else {
-      alert("Printer not available — use iMin Browser and wait for SDK to load.");
+  const handleClick = () => {
+    alert("printing")
+    setIsGreen((prev) => !prev)
+    if (window.Android && window.Android.triggerPrint) {
+      window.Android.triggerPrint();
     }
-  };
+  }
 
-
-  alert((window as any).IminPrintInstance ? "printer ready" : "printer not ready");
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <button
-        onClick={() => handlePrint("SPI")}
-        className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700"
-      >
-        Test SPI
-      </button>
-      <button
-        onClick={() => handlePrint("USB")}
-        className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-      >
-        Test USB
-      </button>
-      <button
-        onClick={() => handlePrint("Bluetooth")}
-        className="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700"
-      >
-        Test Bluetooth
+    <div className="w-full h-screen flex items-center justify-center">
+
+      <div className={`${isGreen ? "bg-green-500" : "bg-red-500"} w-[100px] h-[100px]`}>
+
+      </div>
+
+      <button onClick={handleClick} className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors duration-200" >
+        Print 
       </button>
     </div>
   );
